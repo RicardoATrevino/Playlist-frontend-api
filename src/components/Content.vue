b32b666176cf450ccea813c9e7dbecfa1f4b22c4
Ricardo Trevino <Rickytrevino2380@gmail.com>
2024-04-10 19:09:39 -0700

    mid way through crud

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,8 +1,36 @@
-<script></script>
+<script>
+import axios from "axios";
+import SongsIndex from "./SongsIndex.vue";
+export default {
+  components: {
+    SongsIndex,
+  },
+  data: function () {
+    return {
+      songs: [
+        { id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150 },
+        { id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300 },
+      ],
+    };
+  },
+  created: function () {
+    this.handleIndexSongs();
+  },
+  methods: {
+    handleIndexSongs: function () {
+      axios.get("http://localhost:3000/songs.json").then((response) => {
+        console.log("songs index", response);
+        this.songs = response.data;
+      });
+    },
+  },
+};
+</script>
 
 <template>
   <main>
-    <h1>Welcome to Vue!</h1>
+    <SongsIndex />
+    <SongsIndex v-bind:songs="songs" />
   </main>
 </template>
 
---
+++ src/components/SongsIndex.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    songs: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All Songs</h1>
+    <div v-for="song in songs" v-bind:key="song.id">
+      <h2>{{ song.name }}</h2>
+      <img v-bind:src="song.url" />
+      <p>Width: {{ song.width }}</p>
+      <p>Height: {{ song.height }}</p>
+    </div>
+  </div>
+</template>
+
+<style></style>
