312bb6b1bd8acc5444bfa01164d1765d9e35ea2a
Ricardo Trevino <Rickytrevino2380@gmail.com>
2024-04-11 18:16:02 -0700

    local host 5k and index+stylecss changed

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -1,16 +1,14 @@
 <script>
 import axios from "axios";
 import SongsIndex from "./SongsIndex.vue";
+import "../style.css";
 export default {
   components: {
     SongsIndex,
   },
   data: function () {
     return {
-      songs: [
-        { id: 1, name: "First", url: "https://via.placeholder.com/150", width: 150, height: 150 },
-        { id: 2, name: "Second", url: "https://via.placeholder.com/300", width: 300, height: 300 },
-      ],
+      songs: [],
     };
   },
   created: function () {
@@ -18,10 +16,15 @@ export default {
   },
   methods: {
     handleIndexSongs: function () {
-      axios.get("http://localhost:3000/songs.json").then((response) => {
-        console.log("songs index", response);
-        this.songs = response.data;
-      });
+      axios
+        .get("http://localhost:5000/songs.json")
+        .then((response) => {
+          console.log("songs index", response);
+          this.songs = response.data;
+        })
+        .catch((error) => {
+          console.error("error fetching songs:", error);
+        });
     },
   },
 };
@@ -29,8 +32,7 @@ export default {
 
 <template>
   <main>
-    <SongsIndex />
-    <SongsIndex v-bind:songs="songs" />
+    <SongsIndex v-bind:songs="songs" :class="songs" />
   </main>
 </template>
 
