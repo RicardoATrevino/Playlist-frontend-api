efc6d05d44b8bbbc5a94c3719bca2b765a2d9bfe
Javajunk75 <javajunk75@gmail.com>
2024-04-11 18:39:56 -0700

    your commit message here

---
+++ src/components/SongsIndex.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: {
+    songs: Array,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>All songs</h1>
+    <div v-for="song in songs" v-bind:key="song.id">
+      <h2>{{ song.name }}</h2>
+      <img v-bind:src="song.url" />
+      <p>URL: {{ song.url }}</p>
+      <p>Title: {{ song.title }}</p>
+      <p>Artist: {{ song.artist }}</p>
+      <p>Album: {{ song.Album }}</p>
+      <button v-on:click="$emit('showSong', song)">More info</button>
+    </div>
+  </div>
+</template>
+
+<style></style>
